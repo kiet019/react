f8a320aef86a2f55558fca763af998b00d2cae62
kiet <kietly019@gmail.com>
2023-02-22 21:07:27 +0700

    complete lap4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,18 @@
 import React from "react";
-import { useContext } from 'react'
-import { ThemeContext } from './ThemeContext'
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
 
 export default function Header() {
-  const { theme, toggle} = useContext(ThemeContext)
+  const { theme, toggle } = useContext(ThemeContext);
   return (
-    <div className="header" style={{ backgroundColor: theme.backgroundColor}}>
-      <div className="logo-place">
-      <a className='switch-mode' href="/" onClick={toggle}><span className="material-icons logo">movie</span></a>
-      </div>
+    <div className="header" style={{ backgroundColor: theme.backgroundColor }}>
+      <a className="switch-mode" href="/" onClick={toggle}>
+        <span className="material-icons logo">movie</span>
+      </a>
       <div className="search-box">
-        <form className="search-box-form">
-          <input
-            type="text"
-            className="search-text"
-            placeholder="Your movie name..."
-          />
-          <input type="submit" className="search-submit" value=" "></input>
-          <span className="material-icons search">search</span>
+        <form>
+          <input className="text-input" placeholder="Search your movie..."/>
+            <span className="material-icons search">search</span>
         </form>
       </div>
       <div className="login-user">
