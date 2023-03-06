5606a40c0d0952067a089d32330873d607eac7c9
kiet <kietly019@gmail.com>
2023-03-06 17:12:09 +0700

    lap7

--- src/components/Modal/Details.js
+++ src/components/Modal/Details.js
@@ -2,26 +2,58 @@ import React, { useEffect, useState } from "react";
 import { Icon } from "react-materialize";
 import { useParams } from "react-router-dom";
 import ModalCase from "./ModalCase";
-let ListOfFilms = require("../../shared/ListOfFilms.json");
 
 export default function Details() {
   const [isOpen, setIsOpen] = useState(false);
   const { type } = useParams();
   const { id } = useParams();
-  const listType = ListOfFilms.find((Object) => {
-    return Object.ms === type;
-  });
   // eslint-disable-next-line
-  const film = listType.list.find((Object) => {
-    if (Object.id.toLocaleString() === id) {
-      return Object;
-    }
-  });
+  const baseURL =
+    "https://64055d32eed195a99f80eece.mockapi.io/api/films/details";
+  const [ListOfFilms, setAPIData] = useState([]);
+  const fetchContacts = () => {
+    fetch(baseURL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setAPIData(data);
+      })
+      .catch((error) => console.log(error));
+  };
+  // Hàm để lấy danh sách contact từ API
+  useEffect(() => {
+    fetchContacts();
+  }, []);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [isOpen]);
   return (
     <div className="details">
+      {ListOfFilms.length > 0
+        ? show(
+            ListOfFilms.find((Object) => {
+              // eslint-disable-next-line
+              return Object.ms === type;
+              // eslint-disable-next-line
+            }).list.find((Object) => {
+              if (Object.id.toLocaleString() === id) {
+                return Object;
+              }
+            }),
+            setIsOpen,
+            isOpen
+          )
+        : null}
+    </div>
+  );
+}
+function show(film, setIsOpen, isOpen) {
+  return (
+    <>
       <img src={"../../" + film.image} alt="" />
       <div className="content">
         <h3>Name: {film.title}</h3>
@@ -47,6 +79,6 @@ export default function Details() {
         <Icon>ondemand_video</Icon>
       </a>
       {isOpen && <ModalCase setIsOpen={setIsOpen} film={film} />}
-    </div>
+    </>
   );
 }
